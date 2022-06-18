b2e1442098f2a63bd186c44d43319c2355031af9
hiroki-kondo-git <hiroki.kondo@zuuonline.com>
2022-06-18 23:39:11 +0900

    infra 生成 initial

--- infra/persistence/user.go
+++ infra/persistence/user.go
@@ -1,4 +1,26 @@
 package persistence
 
-// repository という名前にしたいが domain 配下の repository とパッケージ名が被ってしまうため persistence で代替
-// domainロジックの技術的関心を記載
+import (
+	"action/domain/model"
+	"action/domain/repository"
+
+	"github.com/jinzhu/gorm"
+)
+
+type userPersistence struct {
+	Conn *gorm.DB
+}
+
+func NewUserPersistence(conn *gorm.DB) repository.UserRepository {
+	return &userPersistence{Conn: conn}
+}
+
+func (up userPersistence) Insert(name string, gender int) error {
+	// 中身はまだ
+	return nil
+}
+
+func (up userPersistence) GetAllUsersByRoomId(roomId string) ([]*model.User, error) {
+	// 中身まだ
+	return nil, nil
+}
