0ad7ab6fd2638cdad1edf93b091d816a04e713cd
hiroki-kondo-git <hiroki.kondo@zuuonline.com>
2022-06-19 13:32:01 +0900

    依存性注入

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-	rest "action/handler/rest"
+	handler "action/handler"
+	"action/infra/persistence"
+	"action/usecase"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+	// 依存関係の注入
+	userPersistence := persistence.NewUserPersistence()
+	userUseCase := usecase.NewUserUseCase(userPersistence)
+	UserHandler := handler.NewUserHandler(userUseCase)
 
-	e.GET("/", rest.Index)
-	e.GET("/action", rest.Action)
-	e.POST("/users", rest.Users)
+	e.GET("/action", UserHandler.Action)
+	e.POST("/users", UserHandler.UserCreate)
 	e.Logger.Fatal(e.Start(":8080"))
 }
