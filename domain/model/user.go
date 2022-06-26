284d6bab1e18d6e114ae6def29d0f6c1465558b2
hiroki-kondo-git <hiroki.kondo@zuuonline.com>
2022-06-26 18:47:31 +0900

    インスタンス関数

--- domain/model/user.go
+++ domain/model/user.go
@@ -9,8 +9,16 @@ import (
 type Users struct {
 	Id        int    `json:"id"`
 	Name      string `json:"name" validate:"required,gte=1,lte=10,alphanum"`
-	Gender    int    `json:"gender" validate:"required,gte=0,lte=1"`
+	Gender    int    `json:"gender" validate:"required,gte=1,lte=2"`
 	RoomId    string `json:"room_id" validate:"required,gte=10,lte=10"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+func NewUsers(name string, gender int, roomId string) *Users {
+	user := new(Users)
+	user.Name = name
+	user.Gender = gender
+	user.RoomId = roomId
+	return user
 }
